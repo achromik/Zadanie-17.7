f09e9613423092ff966df6b46f025976ecfced2b
Aleksander Chromik <achromik@gmail.com>
2017-10-27 16:43:21 +0200

    INIT - first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '455808925088-t8sffor2aajv92mu7s3jl5jbptn8icpm.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'H-IJcJHgg1It16SIEa-Or-hA',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+const app = express();
+let googleProfile ;
+
+
+/************** start settings for Passport */
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: config.GOOGLE_CLIENT_ID,
+        clientSecret: config.GOOGLE_CLIENT_SECRET,
+        callbackURL: config.CALLBACK_URL
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        // googleProfile = {
+        //     id: profile.id,
+        //     displayName: profile.displayName
+        // };
+
+        googleProfile = Object.assign(profile);
+        cb(null, profile);
+    }
+));
+/************** end settings */
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use('/static', express.static("assets"))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+/*********** app routes */
+app.get('/', (req, res) => {
+     
+    res.render('index', {user: req.user});
+});
+
+app.get('/logged', (req, res) => {
+    console.log()
+    if(googleProfile) {
+        res.render('logged', {user: googleProfile});
+    } else {
+        res.redirect('/error');
+    }
+});
+
+app.get('/logout', (req, res) => {
+    googleProfile = null;
+    req.logOut();
+    res.redirect('/');
+});
+
+
+app.get('/error', (req, res) => {
+    res.render('error');
+});
+
+/********* passport routes */
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect: '/logged',
+        failureRedirect: '/'
+    })
+);
+
+app.use((req, res, next) => {
+    res.redirect('/logged');
+})
+
+app.listen(3000);
+
